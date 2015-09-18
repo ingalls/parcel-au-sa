9be65aac39459824b30da46992e54d159abcbf64
ingalls <nick@mapbox.com>
2015-09-18 18:11:38 +0000

    Switch to au endpoint

--- run.sh
+++ run.sh
@@ -1,88 +1,70 @@
 #!/usr/bin/env bash
 
-# == Avaliable Counties in Georgia =======================
-# Seminole ✓    Clinch     ✓    Glynn    ✓    Cook     - 
-# Thomas   ✓    Ware       ✓    Brantley ✓    Colquitt -
-# Brooks   ✓    Pierce     ✓    Atkinson ✓    Mitchell -
-# Lowndes  ✓    Charlton   ✓    Lanier   ✓    Miller   -
-# Echols   ✓    Camden     -    Berrien  -    Early  
-# Clay          Dougherty       Tift          Irwin
-# Calhoun       Worth           Turner        Coffee
-# Ben Hill      Telfair         Lee           Randolph
-# == Not Avaliable =======================================
-# Decatur
-# Grady
-# Baker
-# ======================================================
-
 if [ -z $1 ]; then
-    echo "./run <County>"
+    echo "./run <GRID #>"
     exit 1
-else
-    COUNTY=$1
 fi
-
-URL="http://qpublic9.qpublic.net/cgi-bin/mapserv60"
-
+GRID=$1
 OLDIFS=$IFS
 
 echo "ok - Setting up build environment"
-mkdir -p /tmp/${COUNTY}_parcels/
-rm /tmp/${COUNTY}_parcels/* &>/dev/null
+mkdir -p /tmp/au_${GRID}_parcels/
+rm -f /tmp/au_${GRID}_parcels/* 
 
-if [ ! -f /tmp/${COUNTY}_bounds ]; then
-    echo "ok - tiling county polygon"
-    while IFS='' read -r line || [[ -n $line ]]; do
-        TOPRIGHT=$(echo $line   | jq -r -c '.geometry | .coordinates | .[] | .[]' | sed '2!d')
-        BOTTOMLEFT=$(echo $line | jq -r -c '.geometry | .coordinates | .[] | .[]' | sed '4!d')
-        echo "[$TOPRIGHT, $BOTTOMLEFT]" >> /tmp/${COUNTY}_bounds
-    done <<< "$( $(dirname $0)/util/cover.js "$(grep "\"13\"" util/county.geojson | grep "$COUNTY" | sed 's/,$//' | jq '.geometry')" | jq -r -c '.features | .[]')"
-else
-    echo "ok - using cached tile polygons"
-fi
+rm /tmp/au_${GRID}_bounds
+echo "ok - tiling grid polygon"
+while IFS='' read -r line || [[ -n $line ]]; do
+    TOPRIGHT=$(echo $line   | jq -r -c '.geometry | .coordinates | .[] | .[]' | sed '2!d')
+    BOTTOMLEFT=$(echo $line | jq -r -c '.geometry | .coordinates | .[] | .[]' | sed '4!d')
+    echo "[$TOPRIGHT, $BOTTOMLEFT]" >> /tmp/au_${GRID}_bounds
+done <<< "$( $(dirname $0)/util/cover.js "$(awk "NR==${GRID}" ./util/grids.geojson | jq '.geometry')" | jq -r -c '.features | .[]')"
 
-echo "ok - Beginning Download ($(wc -l /tmp/${COUNTY}_bounds | grep -Eo "[0-9]+") tiles)"
-cat /tmp/${COUNTY}_bounds | parallel --gnu "$(dirname $0)/util/getImage.sh \"{}\" \"$URL\" \"{#}\" \"$COUNTY\" \"$(wc -l /tmp/${COUNTY}_bounds | grep -Eo "[0-9]+")\""
-rm /tmp/${COUNTY}_bounds
+echo "ok - Beginning Download ($(wc -l /tmp/au_${GRID}_bounds | grep -Eo "[0-9]+") tiles)"
+PROG_TOT=$(wc -l /tmp/au_${GRID}_bounds | grep -Eo "[0-9]+")
+cat /tmp/au_${GRID}_bounds | parallel --gnu "$(dirname $0)/util/getImage.sh \"{}\" \"{#}\" \"$GRID\" \"${PROG_TOT}\""
+
+exit
+
+rm /tmp/au_${GRID}_bounds
 
 echo "ok - merging parcels"
-gdal_merge.py -init 255 -o /tmp/${COUNTY}_parcel_out.tif /tmp/${COUNTY}_parcels/*.tif
-rm -rf /tmp/${COUNTY}_parcels/
+gdal_merge.py -init 255 -o /tmp/${GRID}_parcel_out.tif /tmp/${GRID}_parcels/*.tif
+rm -rf /tmp/${GRID}_parcels/
 
 echo "ok - standarize image"
-convert /tmp/${COUNTY}_parcel_out.tif \
+convert /tmp/${GRID}_parcel_out.tif \
     \( -alpha remove \) \
     \( -fill black -opaque white \) \
-    TIFF64:/tmp/${COUNTY}_parcel_clean.tif
+    TIFF64:/tmp/${GRID}_parcel_clean.tif
 
 echo "ok - set projection"
-./util/gdalcopyproj.py /tmp/${COUNTY}_parcel_out.tif /tmp/${COUNTY}_parcel_clean.tif
-rm /tmp/${COUNTY}_parcel_out.tif
+./util/gdalcopyproj.py /tmp/${GRID}_parcel_out.tif /tmp/${GRID}_parcel_clean.tif
+rm /tmp/${GRID}_parcel_out.tif
 
 echo "ok - gdal_polygonize"
-gdal_polygonize.py -nomask /tmp/${COUNTY}_parcel_clean.tif -f "ESRI Shapefile" /tmp/${COUNTY}_parcel_tile.shp
-rm /tmp/${COUNTY}_parcel_clean.tif
+gdal_polygonize.py -nomask /tmp/${GRID}_parcel_clean.tif -f "ESRI Shapefile" /tmp/${GRID}_parcel_tile.shp
+rm /tmp/${GRID}_parcel_clean.tif
 
 echo "ok - reproject to 4326"
 # Polygonize will be as 54004 even though it is actually 3857
-ogr2ogr /tmp/${COUNTY}_parcel_out.geojson /tmp/${COUNTY}_parcel_tile.shp -s_srs EPSG:3857 -t_srs EPSG:4326 -f "GeoJSON"
-rm /tmp/${COUNTY}_parcel_tile.*
+ogr2ogr /tmp/${GRID}_parcel_out.geojson /tmp/${GRID}_parcel_tile.shp -s_srs EPSG:3857 -t_srs EPSG:4326 -f "GeoJSON"
+rm /tmp/${GRID}_parcel_tile.*
 
 echo "ok - filter by black"
-echo '{ "type": "FeatureCollection", "features": [' > /tmp/${COUNTY}_parcel_pts.geojson.tmp
-grep "DN\": 0" /tmp/${COUNTY}_parcel_out.geojson >> /tmp/${COUNTY}_parcel_pts.geojson.tmp
-sed -i '$s/,$//' /tmp/${COUNTY}_parcel_pts.geojson.tmp
-echo ']}' >> /tmp/${COUNTY}_parcel_pts.geojson.tmp
+echo '{ "type": "FeatureCollection", "features": [' > /tmp/${GRID}_parcel_pts.geojson.tmp
+grep "DN\": 0" /tmp/${GRID}_parcel_out.geojson >> /tmp/${GRID}_parcel_pts.geojson.tmp
+sed -i '$s/,$//' /tmp/${GRID}_parcel_pts.geojson.tmp
+echo ']}' >> /tmp/${GRID}_parcel_pts.geojson.tmp
 
 echo "ok - poly => pt"
-./node_modules/turf-cli/turf-point-on-surface.js /tmp/${COUNTY}_parcel_pts.geojson.tmp > /tmp/${COUNTY}_parcel_pts.geojson
-rm /tmp/${COUNTY}_parcel_pts.geojson.tmp
+./node_modules/turf-cli/turf-point-on-surface.js /tmp/${GRID}_parcel_pts.geojson.tmp > /tmp/${GRID}_parcel_pts.geojson
+rm /tmp/${GRID}_parcel_pts.geojson.tmp
 
 
-jq -r -c '.features | .[] | .geometry | .coordinates' /tmp/${COUNTY}_parcel_pts.geojson > /tmp/${COUNTY}_coords
-PROG_TOT=$(wc -l /tmp/${COUNTY}_parcel_pts.geojson | grep -Po '\d+')
-rm /tmp/${COUNTY}_parcel_pts.geojson
+jq -r -c '.features | .[] | .geometry | .coordinates' /tmp/${GRID}_parcel_pts.geojson > /tmp/${GRID}_coords
+PROG_TOT=$(wc -l /tmp/${GRID}_parcel_pts.geojson | grep -Po '\d+')
+rm /tmp/${GRID}_parcel_pts.geojson
 
-echo "LNG,LAT,STR,DISTRICT,REGION" > ${COUNTY}_out.csv
-cat /tmp/${COUNTY}_coords | parallel -j1 --gnu "./util/getAddress.sh \"{}\" \"{#}\" \"$PROG_TOT\" \"$COUNTY\""
-rm /tmp/${COUNTY}_coords
+echo "LNG,LAT,STR,DISTRICT,REGION" > ${GRID}_out.csv
+cat /tmp/${GRID}_coords | parallel -j1 --gnu "./util/getAddress.sh \"{}\" \"{#}\" \"$PROG_TOT\" \"$GRID\""
+rm /tmp/${GRID}_coords
--- util/getImage.sh
+++ util/getImage.sh
@@ -6,29 +6,32 @@ E=$(echo $1 | sed 's/\\//g' | jq '.[1] | .[0]')
 R=$(echo $1 | sed 's/\\//g' | jq '.[1] | .[1]')
 
 # $1 Bounds [[UPPERLEFT], [LOWERRIGHT]]
-# $2 BaseURL
-# $3 ID
-# $4 County
+# $2 Job #
+# $3 Grid
+# $4 Total Jobs
 
 # Get Map Image
 QUERY=$(echo \
-    "$2" \
-    "?SERVICE=WMS" \
-    "&REQUEST=GetMap" \
-    "&VERSION=1.1.1" \
-    "&LAYERS=parcels" \
-    "&STYLES=" \
-    "&FORMAT=image%2Fpng" \
-    "&TRANSPARENT=true" \
-    "&HEIGHT=512" \
-    "&WIDTH=512" \
-    "&MAP=%2Fvar%2Fwww%2Fmaps%2Fus%2Fregional%2Fqpmap4_mm.map" \
-    "&SRS=EPSG%3A3857" \
-    "&BBOX=$Q,$R,$E,$W" \
+    "http://maps.sa.gov.au/arcgis/rest/services/DPTIExtLandServices/PropertyLocationBrowser20/MapServer/export" \
+    "?dynamicLayers=%5B%7B%22id%22%3A11%2C%22source%22%3A%7B%22type%22%3A%22mapLayer%22%2C%22mapLayerId%22%3A11%7D%2C%22drawingInfo%22%3A%7B%22renderer%22%3A%7B%22type%22%3A%22simple%22%2C%22symbol%22%3A%7B%22color%22%3A%5B145%2C145%2C145%2C51%5D%2C%22outline%22%3A%7B%22color%22%3A%5B0%2C0%2C0%2C51%5D%2C%22width%22%3A0.75%2C%22type%22%3A%22esriSLS%22%2C%22style%22%3A%22esriSLSSolid%22%7D%2C%22type%22%3A%22esriSFS%22%2C%22style%22%3A%22esriSFSSolid%22%7D%7D%7D%7D%2C%7B%22id%22%3A39%2C%22source%22%3A%7B%22type%22%3A%22mapLayer%22%2C%22mapLayerId%22%3A39%7D%2C%22drawingInfo%22%3A%7B%22renderer%22%3A%7B%22type%22%3A%22simple%22%2C%22symbol%22%3A%7B%22color%22%3A%5B145%2C145%2C145%2C51%5D%2C%22outline%22%3A%7B%22color%22%3A%5B0%2C0%2C0%2C51%5D%2C%22width%22%3A0.75%2C%22type%22%3A%22esriSLS%22%2C%22style%22%3A%22esriSLSSolid%22%7D%2C%22type%22%3A%22esriSFS%22%2C%22style%22%3A%22esriSFSSolid%22%7D%7D%7D%7D%5D" \
+    "&dpi=96" \
+    "&format=PNG32" \
+    "&bbox=15384863.632720726%2C-3942146.7790148826%2C15385607.69941804%2C-3941911.4962871596" \
+    "&bboxSR=3857" \
+    "&imageSR=3857" \
+    "&size=1246%2C394" \
+    "&layers=show%3A11%2C39" \
+    "&f=image" \
     | sed 's/ //g'
 )
 
-curl -s $QUERY > /tmp/${4}_${3}.png
+curl -s \
+    -H 'Host: maps.sa.gov.au' \
+    -H 'Referer: http://maps.sa.gov.au/plb/' \
+    "$QUERY" > /tmp/au_${3}_${2}.png
+
+exit
+
 if [[ -z $(which md5sum) ]]; then
     gdal_translate -of GTiff -a_ullr $Q $W $E $R -a_srs 'EPSG:3857' /tmp/${4}_${3}.png /tmp/${4}_parcels/$(md5 -r /tmp/${4}_${3}.png | grep -Eo '.* ' | sed 's/ //g').tif
 else
